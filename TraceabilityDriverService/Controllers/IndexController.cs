5af0d64db00066c18f5b07d7816a50ee202e20e4
Philip Heggelund <59456945+DuckScapePhilip@users.noreply.github.com>
2021-08-20 10:23:15 -0300

    Added in an index GET method on the Traceability Driver.
    Added in an index GET method on the Traceability Driver so that if you go to the base URL it returns "Hello!" so you know it's working properly.

---
+++ TraceabilityDriverService/Controllers/IndexController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraceabilityDriverService.Controllers
+{
+    public class IndexController
+    {
+        [HttpGet]
+        [Route("")]
+        public string Index()
+        {
+            return "Hello!";
+        }
+    }
+}
