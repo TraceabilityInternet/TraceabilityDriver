fa9281b7764b953bb6a711568b4783b717123b6a
John Heggelund <john@pandoscape.com>
2025-04-10 12:01:09 -0500

    Add certificates to common event model Update source and destination models for common event model

--- TraceabilityDriver/Models/Mapping/CommonCertificates.cs
+++ TraceabilityDriver/Models/Mapping/CommonCertificates.cs
@@ -9,6 +9,21 @@ public class CommonCertificates
     public CommonCertificate? FishingAuthorization { get; set; } = null;
 
     /// <summary>
+    /// The harvest certificate for the certificates.
+    /// </summary>
+    public CommonCertificate? HarvestCertification { get; set; } = null;
+
+    /// <summary>
+    /// The chain of custody certification for the certificates.
+    /// </summary>
+    public CommonCertificate? ChainOfCustodyCertification { get; set; } = null;
+
+    /// <summary>
+    /// The human policy certificate for the certificates.
+    /// </summary>
+    public CommonCertificate? HumanPolicyCertificate { get; set; } = null;
+
+    /// <summary>
     /// Merges common certificates into the current context.
     /// </summary>
     /// <param name="other">Contains the certificates to be merged into the existing set.</param>
@@ -21,6 +36,33 @@ public class CommonCertificates
         else if (this.FishingAuthorization != null && other.FishingAuthorization != null)
         {
             this.FishingAuthorization.Merge(other.FishingAuthorization);
+        }
+
+        if (this.HarvestCertification == null && other.HarvestCertification != null)
+        {
+            this.HarvestCertification = other.HarvestCertification;
+        }
+        else if (this.HarvestCertification != null && other.HarvestCertification != null)
+        {
+            this.HarvestCertification.Merge(other.HarvestCertification);
+        }
+
+        if (this.ChainOfCustodyCertification == null && other.ChainOfCustodyCertification != null)
+        {
+            this.ChainOfCustodyCertification = other.ChainOfCustodyCertification;
+        }
+        else if (this.ChainOfCustodyCertification != null && other.ChainOfCustodyCertification != null)
+        {
+            this.ChainOfCustodyCertification.Merge(other.ChainOfCustodyCertification);
+        }
+
+        if (this.HumanPolicyCertificate == null && other.HumanPolicyCertificate != null)
+        {
+            this.HumanPolicyCertificate = other.HumanPolicyCertificate;
+        }
+        else if (this.HumanPolicyCertificate != null && other.HumanPolicyCertificate != null)
+        {
+            this.HumanPolicyCertificate.Merge(other.HumanPolicyCertificate);
         }
     }
 }
